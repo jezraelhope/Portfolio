edf8d5d41c6b0a9453a66c7f30856ec5564262ce
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-11-09 14:26:38 -0600

    About me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -7,9 +7,21 @@ const AboutMe = () => {
 		<div className="about-me" id="about-me">
 			<h2>Hi there, I'm Hope! I'm a self taught front-end developer.</h2>
 			<p>
-			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo similique magnam aspernatur officiis at nam fugiat fuga aperiam amet tenetur. Voluptates minus corporis adipisci at rem amet cupiditate consequatur laudantium.
-			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo similique magnam aspernatur officiis at nam fugiat fuga aperiam amet tenetur. Voluptates minus corporis adipisci at rem amet cupiditate consequatur laudantium.
-			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo similique magnam aspernatur officiis at nam fugiat fuga aperiam amet tenetur. Voluptates minus corporis adipisci at rem amet cupiditate consequatur laudantium.
+			If you told the 10 year old me that she would get into web development, she would have never believed you.
+			</p>
+			<p>
+			I've always thought that this field is only for people who are incredibly smart.
+			Fast foward to the present and I have already created several websites from scratch by teaching myself how to code.
+			</p>
+			<br/>
+			<p>
+			I am fascinated by turning ideas into beautiful, interactive pages and I am in love with the process of creating them.
+			Difficulties sure do arise, but I found that I learn the most during these times.
+			The problem solving aspect of this discipline got me hooked and I am excited for all of the things that I will learn in the future.
+			</p>
+			<br/>
+			<p>
+			When I am not immersed in trying how to center a div, I enjoy painting, interior decorating, reading fantasy books, tending to my plants, annoying my husband and trying to put together an all white puzzle.
 			</p>
 		</div>
 	);
--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,14 +1,37 @@
-import demoImage from "./assets/dpEiCOw.jpeg";
+import nzmDemoImage from "./assets/dpEiCOw.jpeg";
+import wsDashBoard from "./assets/yr8eW1K.jpeg";
+import ylpAnime from "./assets/TcYfqpB.jpeg"
 
 export const projectData = [
 	{
 		id: 1,
-		name: "NZM - A Used Apparel Store",
-		desktopDemo: "placeholder",
-		mobileDemo: [demoImage],
+		name: "NZM",
+		mobileDemo: [nzmDemoImage],
 		liveLink: "http://nzm.netlify.app",
 		githubLink: "https://github.com/jezraelhope/ecomm",
 		description:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aspernatur repudiandae deserunt rem a consequatur consequuntur beatae minus, veritatis asperiores ducimus dignissimos eligendi repellat possimus quia sit perspiciatis fuga iste?",
+			"Carefully curated pieces of clothing and accessories that embody elegance, cleanliness, timelessness and more importantly, sustainability. A used apparel store for someone who cares for the environment.",
 	},
+	{
+		id: 2,
+		name: "Wheel Strategy Dashboard",
+		mobileDemo: [wsDashBoard],
+		liveLink: "https://wheel-strategy-dashboard.onrender.com/",
+		githubLink: "https://github.com/jezraelhope/bambya-dashboard",
+		description:
+			`The Wheel Strategy dashboard is created to display trades made using the options wheel strategy, aiming to easily show profits accrued monthly with the ability to add, edit, close, and delete trades.
+			
+			Technologies used: React, Node, Express, MongoDB, JavaScript, CSS, and HTML
+			`
+	},
+	{
+		id: 3,
+		name: "Anime - Yelp Clone",
+		mobileDemo: [ylpAnime],
+		liveLink: "http://ylp-anime.herokuapp.com/",
+		githubLink: "https://github.com/jezraelhope/yelp-anime",
+		description:
+			`A website created to display a user's favorite anime with the ability to filter according to genre, add, edit, and delete anime if the user is authenticated and also features a show page for anime details including the ability to post reviews, and upvote or downvote, again, if the user is authenticated.
+			`
+	}
 ];
