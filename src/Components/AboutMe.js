8f183829cb9655aee9d8b96015332aaebedd4b0c
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-11-09 21:48:56 -0600

    Changed font family  of p tags

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -6,21 +6,21 @@ const AboutMe = () => {
 	return (
 		<div className="about-me" id="about-me">
 			<h2>Hi there, I'm Hope! I'm a self taught front-end developer.</h2>
-			<p>
+			<p className="about-me-p">
 			If you told the 10 year old me that she would get into web development, she would have never believed you.
 			</p>
-			<p>
+			<p className="about-me-p">
 			I've always thought that this field is only for people who are incredibly smart.
 			Fast foward to the present and I have already created several websites from scratch by teaching myself how to code.
 			</p>
 			<br/>
-			<p>
+			<p className="about-me-p">
 			I am fascinated by turning ideas into beautiful, interactive pages and I am in love with the process of creating them.
 			Difficulties sure do arise, but I found that I learn the most during these times.
 			The problem solving aspect of this discipline got me hooked and I am excited for all of the things that I will learn in the future.
 			</p>
 			<br/>
-			<p>
+			<p className="about-me-p">
 			When I am not immersed in trying how to center a div, I enjoy painting, interior decorating, reading fantasy books, tending to my plants, annoying my husband and trying to put together an all white puzzle.
 			</p>
 		</div>
