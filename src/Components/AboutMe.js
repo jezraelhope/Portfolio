c62230dc5939d3e75778cd655f6376ddc5fd747e
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2022-12-02 22:13:38 -0600

    Edit about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -5,10 +5,7 @@ import "../Styles/aboutme.css";
 const AboutMe = () => {
 	return (
 		<div className="about-me" id="about-me">
-			<h2>Hi there, I'm Hope! I'm a self taught front-end developer.</h2>
-			<p className="about-me-p">
-			If you told the 10 year old me that she would get into web development, she would have never believed you.
-			</p>
+			<h2>Hi there, I'm Hope! I'm a housewife turned front-end developer</h2>
 			<p className="about-me-p">
 			I've always thought that this field is only for people who are incredibly smart.
 			Fast foward to the present and I have already created several websites from scratch by teaching myself how to code.
@@ -16,8 +13,7 @@ const AboutMe = () => {
 			<br/>
 			<p className="about-me-p">
 			I am fascinated by turning ideas into beautiful, interactive pages and I am in love with the process of creating them.
-			Difficulties sure do arise, but I found that I learn the most during these times.
-			The problem solving aspect of this discipline got me hooked and I am excited for all of the things that I will learn in the future.
+			I found that I learn the most when I face a difficult problem. It is so fulfilling when you find a solution to a problem that makes you scratch your head.
 			</p>
 			<br/>
 			<p className="about-me-p">
