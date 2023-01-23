69026845a41a1dc1c2e2d83e668e12ce3f4cedf1
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-23 12:33:32 -0600

    fix skills alt text

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,21 +5,21 @@ import "../Styles/skills.css";
 import skillsData from "../Data/skillsData";
 
 const Skills = () => {
-	return (
-		<div className="skills-and-tools-container" id="skills">
-			<h2 className="skills-and-tools">Skills and Tools</h2>
-			<div className="skills-container">
-				{skillsData.map((item) => {
-					return (
-						<div className="skill" key={item.skill}>
-							<img src={item.image} alt={item.skill} className="skill-icon"/>
-							<p className="skill-name">{item.skill}</p>
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="skills-and-tools-container" id="skills">
+      <h2 className="skills-and-tools">Skills and Tools</h2>
+      <div className="skills-container">
+        {skillsData.map((item) => {
+          return (
+            <div className="skill" key={item.skill}>
+              <img src={item.image} alt="" className="skill-icon" />
+              <p className="skill-name">{item.skill}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
