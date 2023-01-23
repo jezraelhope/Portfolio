14b483b1718aa8284f6e827e890d87b07e0235f9
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-23 12:39:13 -0600

    fix profile image alt text

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,50 +5,53 @@ import hope from "../Data/assets/hope-square.jpg";
 import "../Styles/contact.css";
 
 const Contact = () => {
-	return (
-		<div className="contact-container" id="contact">
-			<div className="contact">
-				<div className="lets-talk">Let's Talk!</div>
-				<div className="contact-content">
-					<form action="https://formsubmit.co/e51707a287b52fbe7105c13830891293" method="POST" className="contact-form">
-						<div className="form-group">
-							<label for="name">Name</label>
-							<input
-								type="text"
-								name="name"
-								className="text-field"
-							/>
-						</div>
-						<div className="form-group">
-							<label for="email">Email</label>
-							<input
-								type="email"
-								name="email"
-								className="text-field"
-							/>
-						</div>
-						<div className="form-group">
-							<label for="message">Message</label>
-							<textarea
-								name="message"
-								className="text-field"
-								id="text-area"
-							/>
-						</div>		
-						<button
-							type="submit"
-							value="Send Email"
-							className="submit"
-							aria-label="submit form button"
-						>Submit</button>
-					</form>
-					<div className="me-and-socials">
-						<img src={hope} alt="my_image" className="my-image" />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="contact-container" id="contact">
+      <div className="contact">
+        <div className="lets-talk">Let's Talk!</div>
+        <div className="contact-content">
+          <form
+            action="https://formsubmit.co/e51707a287b52fbe7105c13830891293"
+            method="POST"
+            className="contact-form"
+          >
+            <div className="form-group">
+              <label for="name">Name</label>
+              <input type="text" name="name" id="name" className="text-field" />
+            </div>
+            <div className="form-group">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="text-field"
+              />
+            </div>
+            <div className="form-group">
+              <label for="text-area">Message</label>
+              <textarea name="message" className="text-field" id="text-area" />
+            </div>
+            <button
+              type="submit"
+              value="Send Email"
+              className="submit"
+              aria-label="submit form button"
+            >
+              Submit
+            </button>
+          </form>
+          <div className="me-and-socials">
+            <img
+              src={hope}
+              alt="a circle with my profile picture"
+              className="my-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
