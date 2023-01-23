9d2cd75c67025ee17ad8c4977edea78ad2625005
Hope Magat <46736323+jezraelhope@users.noreply.github.com>
2023-01-22 21:38:15 -0600

    changed about me section

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -3,19 +3,23 @@ import React from "react";
 import "../Styles/aboutme.css";
 
 const AboutMe = () => {
-	return (
-		<div className="about-me" id="about-me">
-			<h2>Hi there, I'm Hope! I'm a housewife turned front-end developer</h2>
-			<p className="about-me-p">
-			I am fascinated by turning ideas into beautiful, interactive pages. The creation process is so fulfilling to me, and
-			I live for the moments when I face a difficult problem and then finally find the solution and it ends up working!
-			</p>
-			<br/>
-			<p className="about-me-p">
-			When I am not immersed in trying how to center a div, I enjoy painting, interior decorating, reading fantasy books, tending to my plants, annoying my husband and trying to put together an all white puzzle.
-			</p>
-		</div>
-	);
+  return (
+    <div className="about-me" id="about-me">
+      <h2>Hi there, I'm Hope! I'm a self taught front-end developer</h2>
+      <p className="about-me-p">
+        I am fascinated by turning ideas into beautiful, interactive pages. The
+        creation process is so fulfilling to me, and I live for the moments when
+        I face a difficult problem and then finally find the solution and it
+        ends up working!
+      </p>
+      <br />
+      <p className="about-me-p">
+        When I am not immersed in trying how to center a div, I enjoy painting,
+        interior decorating, reading fantasy books, tending to my plants,
+        annoying my husband and trying to put together an all white puzzle.
+      </p>
+    </div>
+  );
 };
 
-export default AboutMe
+export default AboutMe;
